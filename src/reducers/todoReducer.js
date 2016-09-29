4b554f0a0348d110aeb4d36b5ed03755a01ac22b
Mathew Hawley <mathew.hawley@digitaslbi.com>
2016-09-29 14:32:37 +0100

    Split out 'case' reducers for 'todoReducer'

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -3,21 +3,29 @@ import { updateObject, updateObjectInArray } from '../utils/generalUtils';
 
 const INITIAL_STATE = [];
 
+const addTodo = (state, action) => {
+  return [
+    ...state,
+    {
+      text: action.text,
+      id: action.id,
+      completed: false,
+    },
+  ];
+};
+
+const toggleTodo = (state, action) => {
+  return updateObjectInArray(state, action.id, (item) => {
+    return updateObject(item, { completed: !item.completed });
+  });
+};
+
 export const todoReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case constants.ADD_TODO:
-      return [
-        ...state,
-        {
-          text: action.text,
-          id: action.id,
-          completed: false,
-        },
-      ];
+      return addTodo(state, action);
     case constants.TOGGLE_TODO:
-      return updateObjectInArray(state, action.id, (item) => {
-        return updateObject(item, { completed: !item.completed });
-      });
+      return toggleTodo(state, action);
     default:
       return state;
   }

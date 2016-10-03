9337ea16da67af962454f07b239c514a0d0566d8
Mathew Hawley <mathew.hawley@digitaslbi.com>
2016-10-03 12:31:53 +0100

    Update filter reducer imports to use constants

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -1,10 +1,10 @@
-import * as constants from '../constants/filterConstants';
+import { SET_FILTER, FILTERS } from '../constants/filterConstants';
 import { createReducer } from '../utils';
 
-const INITIAL_STATE = 'SHOW_ALL';
+const INITIAL_STATE = FILTERS.SHOW_ALL;
 
 const setFilter = (state, action) => action.filter;
 
 export const filterReducer = createReducer(INITIAL_STATE, {
-  [constants.SET_FILTER]: setFilter,
+  [SET_FILTER]: setFilter,
 });

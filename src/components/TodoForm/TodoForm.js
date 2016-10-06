7bb4c2df9baccc3eb928a96109aacbd4b2b626ff
Mathew Hawley <mathew.hawley@digitaslbi.com>
2016-10-06 13:46:43 +0100

    Extract callback outside of JSX

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -1,20 +1,25 @@
 import React, { PropTypes } from 'react';
 import { addTodoAction } from '../../actions/todoActions';
+import styles from './TodoForm.css';
 
 const TodoForm = ({ dispatch }) => {
+  const keyPresshandler = (event) => {
+    const { charCode, target } = event;
+
+    if (charCode === 13 && target.value.trim()) {
+      dispatch(addTodoAction(target.value));
+      target.value = ''; // eslint-disable-line no-param-reassign
+    }
+  };
+
   return (
     <form onSubmit={(event) => event.preventDefault()}>
       <input
+        className={styles.input}
         type='text'
+        placeholder='Ornare ligula magna inceptos...'
         autoFocus={true}
-        onKeyPress={(event) => {
-          const { charCode, target } = event;
-
-          if (charCode === 13 && target.value.trim()) {
-            dispatch(addTodoAction(target.value));
-            target.value = ''; // eslint-disable-line no-param-reassign
-          }
-        }}
+        onKeyPress={keyPresshandler}
       />
     </form>
   );

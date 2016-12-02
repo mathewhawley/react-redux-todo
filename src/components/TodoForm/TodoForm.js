f337f6666cdcd453a65bf09c0937006a686390c5
Mathew Hawley <mathew@mathewhawley.com>
2016-12-02 19:35:04 +0000

    Update test scripts, general tidy, renaming variables

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -3,33 +3,30 @@ import { addTodoAction } from '../../actions/todoActions';
 import styles from './TodoForm.css';
 
 const TodoForm = ({ dispatch }) => {
-  const keyPressHandler = (event) => {
-    const { target } = event;
-    if (event.charCode === 13 && target.value.trim()) {
-      dispatch(addTodoAction(target.value));
-      target.value = ''; // eslint-disable-line no-param-reassign
-    }
-  };
-
   return (
     <form onSubmit={(event) => event.preventDefault()}>
-        <label
-          id='todo-input-label'
-          htmlFor='todo-input'
-          className='visuallyHidden'
-        >
-          What do you still need to do?
-        </label>
-        <input
-          id='todo-input'
-          className={styles.input}
-          type='text'
-          maxLength={70}
-          placeholder='To do or not to do...'
-          autoFocus={true}
-          onKeyPress={keyPressHandler}
-          aria-labelledby='todo-input-label'
-        />
+      <label
+        id='input-label'
+        htmlFor='input'
+        className='visuallyHidden'
+      >
+        What do you still need to do?
+      </label>
+      <input
+        id='input'
+        className={styles.input}
+        type='text'
+        maxLength={70}
+        placeholder='To do or not to do...'
+        autoFocus={true}
+        onKeyPress={(event) => {
+          if (event.charCode === 13 && event.target.value.trim()) {
+            dispatch(addTodoAction(event.target.value));
+            event.target.value = ''; // eslint-disable-line no-param-reassign
+          }
+        }}
+        aria-labelledby='input-label'
+      />
     </form>
   );
 };
